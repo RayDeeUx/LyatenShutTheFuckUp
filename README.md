8cd1d371129f5b671460541e82ff3214c2a7d780
Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2025-02-07 10:52:02 -0500

    Lyaten, shut the fuck up.

---
+++ README.md
@@ -0,0 +1,8 @@
+# LyatenShutTheFuckUp
+Fun fact! Geode mods don't need to be uploaded to the index!
+
+Another fun fact! Geode mods can be as small and useless as they want!
+
+Final fun fact! Geode mods can target any audience of any size!
+
+This [Geode mod](https://geode-sdk.org) is licensed under LGPLv2.
---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <Geode/modify/FLAlertLayer.hpp>
+
+using namespace geode::prelude;
+
+class $modify(MyFLAlertLayer, FLAlertLayer) {
+	void show() {
+		const auto pl = PlayLayer::get();
+		if (!pl) return FLAlertLayer::show();
+		if (!pl->getParent()) return FLAlertLayer::show();
+		if (!pl->getParent()->getChildByType<PauseLayer>(0)) return FLAlertLayer::show();
+		if (!Mod::get()->getSettingValue<bool>("enabled")) return FLAlertLayer::show();
+		if (!pl->m_isPaused) return FLAlertLayer::show();
+		this->removeMeAndCleanup();
+	}
+};
